2d7c7cf8e3938a4244a2c5ae6e81eebccb89f6ce
Pablo Cubillos <pcubillos326@gmail.com>
2024-02-28 10:38:42 -0500

    adding budget form and validations

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,89 @@
 <script setup>
+import { ref } from 'vue';
+import Filter from './components/Filter.vue'
+import Budget from './components/Budget.vue'
+
+const budget = ref(0)
+
+const defineBudget = (qty) => {
+  budget.value = qty
+}
 </script>
 
 <template>
-  <h1>Template</h1>
+  <div>
+    <header>
+
+      <h1>Expenses Planner</h1>
+      <div class="container-header container shadow">
+        <Budget v-if="budget === 0" @define-budget="defineBudget" />
+      </div>
+    </header>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+:root {
+  --blue: #3b82f6;
+  --white: #FFF;
+  --light-gray: #F5F5F5;
+  --gray: #94a3b8;
+  --dark-gray: #64748b;
+  --black: #000
+}
+
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--light-gray);
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+header {
+  background-color: var(--blue);
+}
+
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--white);
+  text-align: center;
+}
+
+.container {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+
+.container-header {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+
+.shadow {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--white);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
+</style>
---
+++ src/components/Filter.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h2>Filters</h2>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
